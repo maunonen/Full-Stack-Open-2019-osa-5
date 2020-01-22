bacab5f7bdf05f55c40301e639b23f2d4bc62d97
Alexander Maunonen <santari33@gmail.com>
2020-01-22 18:15:04 +0200

    osa 5 note app. Login in frontend

---
+++ note_app_back/build/precache-manifest.b3eef3b82bfb91d503ca0f2562d21a9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83e27c7b91e767e8630d362a155c98a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "203b5152672183b75ad4",
+    "url": "/static/css/main.ddbd2f6b.chunk.css"
+  },
+  {
+    "revision": "b90456a7c0c22d18358c",
+    "url": "/static/js/2.f94fc079.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/static/js/2.f94fc079.chunk.js.LICENSE"
+  },
+  {
+    "revision": "203b5152672183b75ad4",
+    "url": "/static/js/main.7065f020.chunk.js"
+  },
+  {
+    "revision": "8c61276f0d1ed3642cfa",
+    "url": "/static/js/runtime-main.af1e75a3.js"
+  }
+]);
