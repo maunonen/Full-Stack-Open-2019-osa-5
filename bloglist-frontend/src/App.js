aa44d3e3d9ef5ca5f1d01735cc60ff681eb0b290
Alexander Maunonen <santari33@gmail.com>
2020-01-24 12:23:59 +0200

    ex 5.4 bloglist. showMessage()

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -3,19 +3,18 @@ import blogService from './services/blogs'
 import loginService from './services/login'
 import Blog from './components/Blog'
 import BlogForm from './components/BlogForm'
+import Notification from './components/Notification'
 
 const App = (props) => {
 
   const [ username, setUsername ]= useState('')
   const [ password, setPassword ]= useState('')
   const [ user, setUser] = useState( null )
-  const [ errorMessage, setErrorMessage ] = useState( null )
+  const [ notificationMessage, setNotificationMessage ] = useState( null )
   const [ blogs, setBlogs] = useState( null) 
   const [ author, setAuthor] = useState()
   const [ url, setUrl] = useState('')
   const [ title, setTitle] = useState('')
-  
-  
   
   useEffect( () => {
     getBlogs()
@@ -32,6 +31,23 @@ const App = (props) => {
     }
   }, [])
 
+  const showMessage = ( messageType, message, showTime) => {
+    if ( !(typeof messageType === 'string' || messageType instanceof String)){ 
+      return -1
+    } else if (!( typeof showTime === 'number' && showTime > 0 )){
+      return -1
+    } else if ( !(['error', 'successful' ].includes( messageType))){
+      return -1
+    }
+    setNotificationMessage({ 
+          type : messageType, 
+          message : message
+        })
+    setTimeout( () => {
+      setNotificationMessage( null )
+    }, showTime)
+  } 
+
   const getBlogs = async  () => {
       try {
         const blogs = await blogService.getAll()
@@ -39,9 +55,12 @@ const App = (props) => {
           setBlogs( blogs)
         }
       } catch ( err ) {
-        setErrorMessage(`404 not found`)
+        setNotificationMessage({ 
+          type : 'error', 
+          message : '404 not found`'
+        })
         setTimeout( () => {
-          setErrorMessage( null )
+          setNotificationMessage( null )
         }, 5000)
       }
     }
@@ -59,13 +78,9 @@ const App = (props) => {
       setUser( user )
       setUsername('') 
       setPassword('')
-      
+      showMessage('successful', 'You have been successfuly loged in', 5000 )  
     } catch ( err ){
-      
-      setErrorMessage(`Wrong credentials`)
-      setTimeout( () => {
-        setErrorMessage( null )
-      }, 5000)
+      showMessage('error', 'Wrong credentials', 5000 )      
     }
   }
 
@@ -76,7 +91,7 @@ const App = (props) => {
     setUser( null )
     setUsername('') 
     setPassword('')
-    
+    showMessage('successful', 'You have been successfuly loged out', 5000 )
   }
 
   const handleAddBlog = async ( event ) => {
@@ -88,12 +103,11 @@ const App = (props) => {
       setUrl('')
       setTitle('')
       getBlogs()
+      showMessage('successful', 'New blog added', 5000 )
+      
     } catch ( error ){
       console.log('Error', error)
-      setErrorMessage('Can not create blog')
-      setTimeout(() => {
-        setErrorMessage( null )
-      }, 5000)
+      showMessage('error', 'Can not create blog', 5000 )
     }
   }
 
@@ -138,7 +152,8 @@ const App = (props) => {
 
   return (
     <div>
-      <h1>{ errorMessage}</h1>
+      
+      <Notification notification = { notificationMessage}/>
       { 
         user === null 
           ? loginForm() 
--- bloglist-frontend/src/services/login.js
+++ bloglist-frontend/src/services/login.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 const baseurl = '/api/login'
 
 const login = async credentials => {
-    console.log('Credential', credentials)
+    //console.log('Credential', credentials)
     const responce = await axios.post(baseurl, credentials)  
-    console.log('RESPONCE DATA', responce)
+    //console.log('RESPONCE DATA', responce)
     return responce.data
 }
 
